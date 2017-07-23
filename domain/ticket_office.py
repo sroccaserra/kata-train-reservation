ed7c6b8f500af245181a070eb556bff5f8cce015
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-07-23 16:56:03 +0200

    I can reserve n seats in an empty coach

--- domain/ticket_office.py
+++ domain/ticket_office.py
@@ -1,4 +1,10 @@
-
 class TicketOffice:
     def reserve(self, train_id, seat_count):
-        return '{"train_id": "express_2000", "booking_reference": "75bcd15", "seats": ["1A"]}'
+        booking_reference = '75bcd15'
+        seats = ['{0}A'.format(x) for x in range(1, seat_count + 1)]
+
+        return {
+            'train_id': train_id,
+            'booking_reference': booking_reference,
+            'seats': seats
+        }
--- test/test_ticket_office.py
+++ test/test_ticket_office.py
@@ -4,10 +4,21 @@ from domain.ticket_office import TicketOffice
 
 
 class TrainsWithOneCoach(TestCase):
-    def test_reserve_one_seat_in_empty_coach(self):
-        ticket_office = TicketOffice()
-        expected_reservation =\
-            '{"train_id": "express_2000", "booking_reference": "75bcd15", "seats": ["1A"]}'
+    def build_reservation(self, nb_seats):
+        reservation_with_one_seat = {
+            'train_id': 'express_2000',
+            'booking_reference': '75bcd15',
+            'seats': ['{0}A'.format(x) for x in range(1, nb_seats + 1)]
+        }
+        return reservation_with_one_seat
 
-        self.assertEqual(expected_reservation,
-                         ticket_office.reserve('express_2000', 1))
+    def setUp(self):
+        self.ticket_office = TicketOffice()
+
+    def test_reserve_one_seat_in_empty_coach(self):
+        self.assertEqual(self.build_reservation(1),
+                         self.ticket_office.reserve('express_2000', 1))
+
+    def test_reserve_two_seats_in_empty_coach(self):
+        self.assertEqual(self.build_reservation(2),
+                         self.ticket_office.reserve('express_2000', 2))
