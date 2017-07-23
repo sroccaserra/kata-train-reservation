08c908628d93103dbc4de229d069cc6976d8c5e8
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-07-23 11:47:01 +0200

    Faked reserve one seat in empty coach

--- domain/ticket_office.py
+++ domain/ticket_office.py
@@ -1,4 +1,4 @@
 
 class TicketOffice:
     def reserve(self, train_id, seat_count):
-        raise NotImplementedError()
+        return '{"train_id": "express_2000", "booking_reference": "75bcd15", "seats": ["1A"]}'
--- test/test_ticket_office.py
+++ test/test_ticket_office.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 
+from domain.ticket_office import TicketOffice
 
-class TestTicketOfficeShould(TestCase):
-    def test_fails(self):
-        self.assertEqual(2, 3)
+
+class TrainsWithOneCoach(TestCase):
+    def test_reserve_one_seat_in_empty_coach(self):
+        ticket_office = TicketOffice()
+        expected_reservation =\
+            '{"train_id": "express_2000", "booking_reference": "75bcd15", "seats": ["1A"]}'
+
+        self.assertEqual(expected_reservation,
+                         ticket_office.reserve('express_2000', 1))
